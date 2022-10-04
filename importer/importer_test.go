4b4ec6bf7ec47d8d1a5fcaf49daeb98c073e3671
Mark Sturdevant <mark.sturdevant@ibm.com>
2022-10-04 14:38:24 -0700

    Importer/validate test, troubleshoot, refactor
    * Some importer/validator refactoring
    * Tests for known problems (result is skip)
      - this is where problems can repro/fail, fix/pass
    * Don't fail on first error. Log, count and fail at end.
    * Copy files with template errors to testdata/invalid (for handy troubleshooting and listing)
    * Add debug code for template match problems
    * Fix filemode to 0o600 as appropriate for git
    
    Signed-off-by: Mark Sturdevant <mark.sturdevant@ibm.com>

---
+++ importer/importer_test.go
@@ -0,0 +1,64 @@
+package importer
+
+import (
+	"os"
+	"path"
+	"testing"
+)
+
+func TestImporter_Validate(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		id      string
+		wantErr bool
+	}{
+		{
+			name:    "0BSD should validate out-of-the-box",
+			id:      "0BSD",
+			wantErr: false,
+		},
+		{
+			name:    "BlueOak-1.0.0 omitable line prefix ## needs to be normalized like line comment",
+			id:      "BlueOak-1.0.0",
+			wantErr: true,
+		},
+		{
+			name:    "CC-BY-3.0 space before comma",
+			id:      "CC-BY-3.0",
+			wantErr: true,
+		},
+		{
+			name:    "CC-BY-NC-SA-2.0-FR template mod removed several extra ' . '",
+			id:      "CC-BY-NC-SA-2.0-FR",
+			wantErr: false,
+		},
+	}
+	testData := "../testdata/validator"
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			id := tt.id
+			templateFile := path.Join(testData, id+".template.txt")
+			templateBytes, err := os.ReadFile(templateFile)
+			if err != nil {
+				t.Errorf("ID: %v Read template file error: %v", id, err)
+				return
+			}
+			textFile := path.Join(testData, id+".txt")
+			textBytes, err := os.ReadFile(textFile)
+			if err != nil {
+				t.Errorf("ID: %v Read text file error: %v", id, err)
+				return
+			}
+			if _, err := validate(id, templateBytes, textBytes, templateFile); err != nil {
+				if tt.wantErr == true {
+					t.Skipf("validate() error = %v, wantErr %v", err, tt.wantErr)
+				} else {
+					t.Errorf("validate() error = %v, wantErr %v", err, tt.wantErr)
+				}
+			}
+		})
+	}
+}
--- importer/validator.go
+++ importer/validator.go
@@ -7,139 +7,111 @@ import (
 	"os"
 	"path"
 
+	"github.com/mrutkows/sbom-utility/log"
+
+	"github.com/IBM/license-scanner/debugger"
 	"github.com/IBM/license-scanner/identifier"
 	"github.com/IBM/license-scanner/licenses"
 	"github.com/IBM/license-scanner/normalizer"
-
-	"golang.org/x/exp/slices"
 )
 
-func ValidateSPDXTemplateWithLicenseText(id, templateFile, textFile, templateDestDir, preCheckDestDir, textDestDir string) (bool, error) {
-	const isValid = false // return false for all skips and errors -- only return true at the very end
+func ValidateSPDXTemplateWithLicenseText(id, templateFile, textFile, templateDestDir, preCheckDestDir, textDestDir string) (err error) {
+	var templateBytes []byte
+	var textBytes []byte
+	var staticBlocks []string
 
-	// Skipping known SPDX template/text issues.  Some here. More in later steps below.
-	skips := []string{
-		"deprecated_Nokia-Qt-exception-1.1", // no test file
-	}
+	// on error, save template/text/precheck files (if available) under testdata/invalid
+	defer func() {
+		if err != nil {
+			invalid := path.Join(textDestDir, "invalid") // on error save files in testdata/invalid
+			_ = os.Mkdir(invalid, 0o700)
+			_ = write(id, invalid, templateBytes, invalid, textBytes, invalid, staticBlocks)
+		}
+	}()
 
-	if slices.Contains(skips, id) {
-		return isValid, nil
-	}
-
-	textBytes, err := os.ReadFile(textFile)
+	textBytes, err = os.ReadFile(textFile)
 	if err != nil {
-		return isValid, err
+		return
 	}
+	templateBytes, err = os.ReadFile(templateFile)
+	if err != nil {
+		return
+	}
+
+	staticBlocks, err = validate(id, templateBytes, textBytes, templateFile)
+	if err != nil {
+		return err
+	}
+
+	if err = write(id, templateDestDir, templateBytes, textDestDir, textBytes, preCheckDestDir, staticBlocks); err != nil {
+		return
+	}
+	return
+}
+
+func validate(id string, templateBytes []byte, textBytes []byte, templateFile string) (staticBlocks []string, err error) {
+
+	l := &licenses.License{}
+	if err = licenses.AddPrimaryPatternAndSource(string(templateBytes), templateFile, l); err != nil {
+		return
+	}
+
+	if _, err = licenses.GenerateMatchingPatternFromSourceText(l.PrimaryPatterns[0]); err != nil {
+		return
+	}
+
 	normalizedTestData := normalizer.NormalizationData{
 		OriginalText: string(textBytes),
 	}
-	if err := normalizedTestData.NormalizeText(); err != nil {
-		return isValid, err
-	}
-
-	templateBytes, err := os.ReadFile(templateFile)
-	if err != nil {
-		return isValid, err
-	}
-
-	l := &licenses.License{}
-	if err := licenses.AddPrimaryPatternAndSource(string(templateBytes), templateFile, l); err != nil {
-		return isValid, err
-	}
-
-	if _, err := licenses.GenerateMatchingPatternFromSourceText(l.PrimaryPatterns[0]); err != nil {
-		return isValid, err
-	}
-
-	// SPDX template/text issues
-	skips = []string{
-		"Afmparse", // missing space
-		"BlueOak-1.0.0",
-		"CC-BY-3.0",
-		"CC-BY-NC-SA-2.0-FR",
-		"CC-BY-SA-3.0",
-		"CECILL-2.0",
-		"CECILL-2.1",
-		"CECILL-B",
-		"CECILL-C",
-		"COIL-1.0",
-		"Community-Spec-1.0",
-		"D-FSL-1.0",
-		"EPL-1.0",
-		"EUDatagrid",
-		"EUPL-1.2",
-		"Elastic-2.0",
-		"ErlPL-1.1",
-		"FreeImage",
-		"IBM-pibs",
-		"LAL-1.2",
-		"LAL-1.3",
-		"LPL-1.0",
-		"LZMA-SDK-9.11-to-9.20",
-		"LZMA-SDK-9.22",
-		"LiLiQ-Rplus-1.1",
-		"MulanPSL-1.0",
-		"Multics",
-		"NCGL-UK-2.0",
-		"NPL-1.1",
-		"OGL-UK-1.0",
-		"OSET-PL-2.1",
-		"Parity-7.0.0",
-		"PolyForm-Noncommercial-1.0.0",
-		"PolyForm-Small-Business-1.0.0",
-		"SHL-2.0",
-		"SHL-2.1",
-		"SSPL-1.0",
-		"W3C-19980720",
-		"copyleft-next-0.3.0",
-		"copyleft-next-0.3.1",
-		"iMatix",
+	if err = normalizedTestData.NormalizeText(); err != nil {
+		return
 	}
 
 	matches, err := identifier.FindMatchingPatternInNormalizedData(l.PrimaryPatterns[0], normalizedTestData)
 	if err != nil {
-		return isValid, err
+		return
 	}
 
 	// There should be exactly ONE match when matching a template against its example license text
 	if len(matches) != 1 {
 		err = Logger.Errorf("expected 1 match for %v got: %v", id, matches)
-		if slices.Contains(skips, id) {
-			return isValid, nil // skip with known err
-		} else {
-			return isValid, err
+		if Logger.GetLevel() >= log.DEBUG {
+			failure, _ := debugger.DebugLicenseMatchFailure(*l, normalizedTestData.NormalizedText)
+			Logger.Debug(fmt.Errorf("Debugging invalid template for %v...\n", id))
+			Logger.Debug(failure)
+			Logger.Debug("\n")
 		}
+		return
 	}
 
-	// Normalize the input text.
 	normalizedTemplate := &normalizer.NormalizationData{
 		OriginalText: string(templateBytes),
 	}
-	err = normalizedTemplate.NormalizeText()
-	if err != nil {
-		return isValid, err
+	if err = normalizedTemplate.NormalizeText(); err != nil {
+		return
 	}
 
-	staticBlocks := GetStaticBlocks(normalizedTemplate)
+	staticBlocks = GetStaticBlocks(normalizedTemplate)
 	passed := identifier.PassedStaticBlocksChecks(staticBlocks, normalizedTestData)
 	if !passed {
-		return isValid, Logger.Errorf("%v failed testing against static blocks", id)
+		err = Logger.Errorf("%v failed testing against static blocks", id)
+		return
+	}
+	return
+}
+
+func write(id string, templateDestDir string, templateBytes []byte, textDestDir string, textBytes []byte, preCheckDestDir string, staticBlocks []string) error {
+
+	if err := os.WriteFile(path.Join(templateDestDir, id+".template.txt"), templateBytes, 0o600); err != nil {
+		return Logger.Errorf("error writing template for %v: %w", id, err)
 	}
 
-	err = WritePreChecksFile(staticBlocks, path.Join(preCheckDestDir, id+".json"))
-	if err != nil {
-		return isValid, err
+	if err := os.WriteFile(path.Join(textDestDir, id+".txt"), textBytes, 0o600); err != nil {
+		return Logger.Errorf("error writing testdata for %v: %w", id, err)
 	}
 
-	err = os.WriteFile(path.Join(templateDestDir, id+".template.txt"), templateBytes, 0o666) //nolint:gosec
-	if err != nil {
-		return isValid, fmt.Errorf("error writing template for %v: %w", id, err)
+	if err := WritePreChecksFile(staticBlocks, path.Join(preCheckDestDir, id+".json")); err != nil {
+		return Logger.Errorf("error writing precheck file for %v: %w", id, err)
 	}
-
-	err = os.WriteFile(path.Join(textDestDir, id+".txt"), textBytes, 0o666) //nolint:gosec
-	if err != nil {
-		return isValid, fmt.Errorf("error writing testdata for %v: %w", id, err)
-	}
-
-	return true, nil
+	return nil
 }
